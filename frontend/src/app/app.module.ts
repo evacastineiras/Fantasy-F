d793102f4e8820493c4918a4275ccaa744a3c4b4
evacastineiras <evacpuente@gmail.com>
2025-08-05 18:42:50 +0200

    Landing page base

--- frontend/src/app/app.module.ts
+++ frontend/src/app/app.module.ts
@@ -2,10 +2,12 @@ import { NgModule } from '@angular/core';
 import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
+import { LandingBaseComponent } from './landing/landing-base/landing-base.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LandingBaseComponent
   ],
   imports: [
     BrowserModule
---
+++ frontend/src/app/landing/landing-base/landing-base.component.ts
@@ -0,0 +1,15 @@
+import { Component, OnInit } from '@angular/core';
+
+@Component({
+  selector: 'app-landing-base',
+  templateUrl: './landing-base.component.html',
+  styleUrls: ['./landing-base.component.css']
+})
+export class LandingBaseComponent implements OnInit {
+
+  constructor() { }
+
+  ngOnInit(): void {
+  }
+
+}
